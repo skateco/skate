11e9ed491a1eadb4175bfa201dee437746ab161e
Donal Byrne <byrnedo@tcd.ie>
2023-10-27 15:57:24 +0200

    Loads yaml

--- src/skate.rs
+++ src/skate.rs
@@ -47,7 +47,7 @@ pub async fn skate() -> Result<(), Box<dyn Error>> {
 
 fn apply(args: ApplyArgs) -> Result<(), Box<dyn Error>> {
     let hosts = read_hosts(args.hosts.hosts_file)?;
-    // let merged_config = read_config(args.filename)?;
+    let merged_config = read_config(args.filename)?; // huge
     // let game_plan = schedule(merged_config, hosts)?;
     // game_plan.play()
     Ok(())
@@ -63,8 +63,42 @@ struct Hosts {
     hosts: Vec<Host>,
 }
 
+
 fn read_hosts(hosts_file: String) -> Result<Hosts, Box<dyn Error>> {
     let f = std::fs::File::open(".hosts.yaml")?;
     let data: Hosts = serde_yaml::from_reader(f)?;
     Ok(data)
+}
+
+fn read_config(filenames: Vec<String>) -> Result<(), Box<dyn Error>> {
+    let api_version_key = serde_yaml::Value::String("apiVersion".to_owned());
+    let kind_key = serde_yaml::Value::String("kind".to_owned());
+
+    for filename in filenames {
+        let file = std::fs::File::open(filename)?;
+        let file: serde_yaml::Sequence = serde_yaml::from_reader(file)?;
+        for document in file {
+            if let serde_yaml::Value::Mapping(mapping) = &document {
+                let api_version = mapping.get(&api_version_key).and_then(serde_yaml::Value::as_str);
+                let kind = mapping.get(&kind_key).and_then(serde_yaml::Value::as_str);
+                match (api_version, kind) {
+                    (Some(api_version), Some(kind)) if
+                    api_version == <k8s_openapi::api::core::v1::Pod as k8s_openapi::Resource>::API_VERSION &&
+                        kind == <k8s_openapi::api::core::v1::Pod as k8s_openapi::Resource>::KIND =>
+                        {
+                            let pod: k8s_openapi::api::core::v1::Pod = serde::Deserialize::deserialize(document)?;
+                        }
+
+                    (Some(api_version), Some(kind)) if
+                    api_version == <k8s_openapi::api::apps::v1::Deployment as k8s_openapi::Resource>::API_VERSION &&
+                        kind == <k8s_openapi::api::apps::v1::Deployment as k8s_openapi::Resource>::KIND =>
+                        {
+                            let deployment: k8s_openapi::api::apps::v1::Deployment = serde::Deserialize::deserialize(document)?;
+                        }
+                    _ => {}
+                }
+            }
+        }
+    }
+    Ok(())
 }
