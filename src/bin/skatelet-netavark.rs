7b723134e15e0c8464b011aa69810d9c1c80b85e
Donal Byrne <byrnedo@tcd.ie>
2024-08-17 00:16:49 +0200

    WIP

--- src/bin/skatelet-netavark.rs
+++ src/bin/skatelet-netavark.rs
@@ -1,10 +1,10 @@
-#![cfg(target_os = "linux")]
-
 use std::error::Error;
+#[cfg(target_os = "linux")]
 use skate::netavark;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    #[cfg(target_os = "linux")]
     netavark();
     Ok(())
 }
