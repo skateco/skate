217142d8b752bfd9b7cba15f270c7b6879cf9259
Donal Byrne <byrnedo@tcd.ie>
2024-09-26 15:00:47 +0200

    wip

--- src/controllers/pod.rs
+++ src/controllers/pod.rs
@@ -4,17 +4,24 @@ use k8s_openapi::api::core::v1::Pod;
 use crate::skate::{exec_cmd, SupportedResources};
 use crate::util::apply_play;
 
-pub struct PodController {
-}
+pub struct PodController {}
 
 impl PodController {
     pub fn new() -> Self {
-        PodController {
-        }
+        PodController {}
     }
 
     pub fn apply(&self, pod: Pod) -> Result<(), Box<dyn Error>> {
-        apply_play(SupportedResources::Pod(pod))
+        self.apply_quadlet(pod)
+        //apply_play(SupportedResources::Pod(pod))
+    }
+
+    fn apply_quadlet(&self, pod: Pod) -> Result<(), Box<dyn Error>> {
+        // use quadlet to convert the pod manifest to a .container file
+        // /usr/libexec/podman/quadlet
+        // the file needs to be put in /etc/containers/systemd/
+        // then alter anything we need then start it
+        Ok(())
     }
 
     pub fn delete(&self, pod: Pod, grace_period: Option<usize>) -> Result<(), Box<dyn Error>> {
