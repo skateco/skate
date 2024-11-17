623d98758dfb932835faf12be3d5e115a26d92e3
Donal Byrne <byrnedo@tcd.ie>
2024-11-17 20:24:39 +0100

    Added get-contexts/get-clusters

--- src/config_cmd.rs
+++ src/config_cmd.rs
@@ -19,18 +19,27 @@ pub struct UseContextArgs{
 
 #[derive(Debug, Subcommand)]
 pub enum ConfigCommands {
+    GetClusters,
+    GetContexts,
     CurrentContext,
     UseContext(UseContextArgs),
 }
 
 pub fn config(args: ConfigArgs) -> Result<(), SkateError> {
     match args.command {
+        ConfigCommands::GetContexts | ConfigCommands::GetClusters => {
+            let config = crate::config::Config::load(Some(args.config.skateconfig.clone()))?;
+            println!("NAME");
+            for ctx in config.clusters {
+                println!("{}", ctx.name)
+            }
+        },
         ConfigCommands::CurrentContext => {
-            let config = crate::config::Config::load(Some(args.config.skateconfig.clone())).expect("failed to load skate config");
+            let config = crate::config::Config::load(Some(args.config.skateconfig.clone()))?;
             println!("{}", config.current_context.unwrap_or_default())
         },
         ConfigCommands::UseContext(use_context_args) => {
-            let mut config = crate::config::Config::load(Some(args.config.skateconfig.clone())).expect("failed to load skate config");
+            let mut config = crate::config::Config::load(Some(args.config.skateconfig.clone()))?;
             config.clusters.iter().any(|c| c.name == use_context_args.context)
                 .then_some(())
                 .ok_or(anyhow!("no context exists with the name {}", use_context_args.context))?;
