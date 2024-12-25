b1863442d312b9ae408b4df0fb10c4aad8e3afe4
Donal Byrne <byrnedo@tcd.ie>
2024-12-25 23:58:14 +0100

    WIP

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+.ONE_SHELL:
 .PHONY: default
 default: aarch64
 
@@ -23,11 +24,14 @@ lint-fix:
 
 
 .PHONY: run-integration-tests
+run-integration-tests: SSH_PRIVATE_KEY=/tmp/skate-e2e-key
 run-integration-tests:
+	[ -f ${SSH_PRIVATE_KEY} ] || ssh-keygen -b 2048 -t rsa -f ${SSH_PRIVATE_KEY} -q -N ""
+	echo "SSH_PRIVATE_KEY=${SSH_PRIVATE_KEY}" > ./hack/.clusterplz.env
 	# start vms
 	./hack/clusterplz create || exit 0
 	# This copies over a skatelet binary. Not good, should really find a better way to do this.
 	./hack/clusterplz skatelet
     # the ignored tests are the integration tests. This is not optimal.
-	cargo test -- --include-ignored
+	cargo test --test '*' -v -- --show-output --nocapture --include-ignored
 
--- tests/e2e_test.rs
+++ tests/e2e_test.rs
@@ -1,6 +1,9 @@
-use std::{panic, process};
+use std::{env, panic, process};
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter};
+use std::io::{stderr, stdout};
+use std::process::Stdio;
+use serde::{Deserialize, Serialize};
 
 fn setup() {}
 
@@ -32,33 +35,45 @@ impl Display for SkateError {
     }
 }
 
-fn skate(command: &str, args: &[&str]) -> Result<String, SkateError> {
-    let output = process::Command::new("./target/debug/skate")
+fn skate(command: &str, args: &[&str]) -> Result<(), SkateError> {
+    let mut child = process::Command::new("./target/debug/skate")
         .args([&[command], args].concat())
-        .output().map_err(|e| SkateError { exit_code: -1, message: e.to_string() })?;
-    if !output.status.success() {
-        return Err(SkateError { exit_code: output.status.code().unwrap_or_default(), message: String::from_utf8_lossy(&output.stderr).to_string() });
+        .stdout(stdout())
+        .stderr(stderr())
+        .spawn().map_err(|e| SkateError { exit_code: -1, message: e.to_string() })?;
+
+
+    let status = child.wait().map_err(|e| SkateError{exit_code: -1, message: e.to_string()})?;
+    if !status.success() {
+        return Err(SkateError { exit_code: status.code().unwrap_or_default(), message:"".to_string() });
     }
 
-    Ok(String::from_utf8_lossy(&output.stdout).trim_end().into())
+    Ok(())
 }
 
 fn ips() -> Result<(String, String), anyhow::Error> {
-    let output = process::Command::new("../hack/clusterplz")
-        .args(&["ips"])
+    let output = process::Command::new("multipass")
+        .args(&["info", "--format", "json"])
         .output()?;
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    let mut lines = stdout.lines();
-    Ok((lines.nth(0).unwrap().into(), lines.nth(0).unwrap().into()))
+
+    let info: serde_json::Value = serde_json::from_slice(&output.stdout)?;
+    let node1 = info["info"]["node-1"]["ipv4"][0].as_str().ok_or(anyhow::anyhow!("failed to get node-1 ip"))?;
+    let node2 = info["info"]["node-2"]["ipv4"][0].as_str().ok_or(anyhow::anyhow!("failed to get node-2 ip"))?;
+
+    Ok((node1.to_string(), node2.to_string()))
 }
 
 #[test]
 #[ignore]
 fn test_node_creation() -> Result<(), anyhow::Error> {
     let ips = ips()?;
-    skate("delete", &["cluster", "integration-test", "--force"])?;
-    skate("create", &["cluster", "integration-test"])?;
+
+    let user = env::var("USER")?;
+
+    //skate("delete", &["cluster", "integration-test", "--force"])?;
+    skate("create", &["cluster", "integration-test"]);
     skate("config", &["use-context", "integration-test"])?;
-     skate("create", &["node", "--name", "node-1", "--host", "192.168.1.99", "--subnet-cidr", "20.1.0.0/16"])?;
+    skate("create", &["node", "--name", "node-1", "--host", &ips.0, "--subnet-cidr", "20.1.0.0/16", "--key", "/tmp/skate-e2e-key", "--user", &user])?;
+    skate("create", &["node", "--name", "node-2", "--host", &ips.1, "--subnet-cidr", "20.2.0.0/16", "--key", "/tmp/skate-e2e-key", "--user", &user])?;
     Ok(())
 }
