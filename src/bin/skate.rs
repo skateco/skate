9fc5560ff7fc4b40a19a02b3af1aacf7bf1253c0
Donal Byrne <byrnedo@tcd.ie>
2025-01-08 23:17:02 +0100

    Pretty print errors (#37)

--- src/bin/skate.rs
+++ src/bin/skate.rs
@@ -1,12 +1,19 @@
 #![warn(unused_extern_crates)]
 
 use skate::deps::Deps;
-use skate::errors::SkateError;
 use skate::{skate};
 
 #[tokio::main]
-async fn main() -> Result<(), SkateError> {
+async fn main() {
 
     let deps = Deps {};
-    skate(deps).await
+    let res = skate(deps).await;
+
+    match res {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("{}", e.to_string());
+            std::process::exit(1);
+        }
+    }
 }
