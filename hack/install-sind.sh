c0ce4bcbbdfccafb438d1e41a3eee1a1018d9d2d
Donal Byrne <byrnedo@tcd.ie>
2025-04-20 21:30:47 +0200

    Sind install script

---
+++ hack/install-sind.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+set -euo pipefail
+
+# debug
+if [[ -n "${DEBUG:-}" ]]; then
+  set -x
+fi
+
+os=$(uname -o)
+arch=$(uname -m)
+
+# make these more elegant later
+if [[ "$os" == "GNU/Linux" ]]; then
+  os="unknown-linux-gnu"
+elif [[ "$os" == "Darwin" ]]; then
+  os="apple-darwin"
+else
+  echo "Unsupported OS: $os"
+  exit 1
+fi
+
+# make these more elegant later
+if [[ "$arch" == *"x86_64"* ]]; then
+  arch="x86_64"
+elif [[ "$arch" == *"aarch64"* ]]; then
+  arch="aarch64"
+elif [[ "$arch" == *"arm64"* ]]; then
+  arch="aarch64"
+else
+  echo "Unsupported architecture: $arch"
+  exit 1
+fi
+
+get_install_alternatives(){
+  curl -s https://api.github.com/repos/skateco/skate/releases/latest \
+    | grep "browser_download_url.*tar.gz" \
+    | cut -d : -f 2,3 \
+    | tr -d \\\" \
+    | tr -d "[:blank:]"
+}
+
+# Find for our triple
+
+triple="$arch-$os"
+echo "Triple: $triple"
+
+archive_name="sind-$triple.tar.gz"
+
+install_url=$(get_install_alternatives|grep "$archive_name" | head -n 1)
+
+if [[ -z "$install_url" ]]; then
+  echo "No install URL found for $archive_name"
+  exit 1
+fi
+
+echo "Installing from $install_url"
+
+rm -rf /tmp/sind-install
+mkdir -p /tmp/sind-install
+curl -sSL -o "/tmp/sind-install/sind.tar.gz" "$install_url"
+
+cd /tmp/sind-install
+tar -xvf sind.tar.gz
+
+
+
+sudo mv sind /usr/local/bin/sind
+sudo chmod +x /usr/local/bin/sind
+
+echo "Sind installed successfully in /usr/local/bin"
+
