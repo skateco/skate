2ed9684755d4af13fd9a6ea836a71ba1ae99bfc3
Donal Byrne <byrnedo@tcd.ie>
2025-04-28 19:37:00 +0200

    WIP

---
+++ migrations/20250426185755_initial.sql
@@ -0,0 +1,10 @@
+CREATE TABLE resources (
+    id VARCHAR(36) PRIMARY KEY, -- uuid
+    name VARCHAR(255) NOT NULL,
+    namespace VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    manifest JSONB NOT NULl,
+    hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
