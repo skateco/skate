69430a482806a66807fa0ac1f4cb75dcf1503dee
Donal Byrne <byrnedo@tcd.ie>
2025-05-06 22:06:42 +0200

    Fix skatelet binary install

--- src/sind/create.rs
+++ src/sind/create.rs
@@ -150,6 +150,8 @@ pub async fn create<D: CreateDeps>(deps: D, main_args: CreateArgs) -> Result<(),
         )?;
 
         if let Some(skatelet_path) = &main_args.skatelet_binary_path {
+            shell_exec.exec_stdout("docker", &["exec", &name, "mkdir", "-p", "/var/lib/skate"])?;
+
             println!("Copying skatelet binary to node {}", name);
             shell_exec.exec_stdout(
                 "docker",
