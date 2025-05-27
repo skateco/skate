55a004c3f864ffea95a84df76f64ee2ef4cd0948
Donal Byrne <byrnedo@tcd.ie>
2025-05-27 23:39:58 +0200

    WIP

--- src/scheduler/score.rs
+++ src/scheduler/score.rs
@@ -4,6 +4,23 @@ use std::collections::HashMap;
 
 pub enum Error {}
 
+//*
+// NOTE: the plugin system is inspired by the Kubernetes scheduler plugin system.
+
+// Copyright 2019 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// */
 fn pod_priority(pod: &Pod) -> i32 {
     pod.spec
         .as_ref()
@@ -54,4 +71,8 @@ pub trait Score {
 /// network interface or preparing a volume
 pub trait PreBind {
     fn pre_bind(pod: &Pod, node: &NodeState) -> Result<(), Error>;
+}
+
+pub trait PostBind {
+    fn post_bind(pod: &Pod, node: &NodeState) -> Result<(), Error>;
 }
