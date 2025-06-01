cd6caa89ca3d4bb70f6896e2dea0424d10c3816e
Donal Byrne <byrnedo@tcd.ie>
2025-06-01 23:05:41 +0200

    WIP

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -4,15 +4,11 @@ mod least_pods;
 mod node_name;
 mod node_resources_fit;
 mod plugins;
+mod pod_scheduler;
 mod priority_sort;
 mod unschedulable;
 
-use crate::scheduler::filter::NodeSelectorFilter;
-use crate::scheduler::least_pods::LeastPods;
-use crate::scheduler::node_name::NodeNameFilter;
-use crate::scheduler::node_resources_fit::NodeResourcesFit;
-use crate::scheduler::priority_sort::PrioritySort;
-use crate::scheduler::unschedulable::UnschedulableFilter;
+use crate::scheduler::pod_scheduler::PodScheduler;
 use crate::skatelet::database::resource::ResourceType;
 use crate::skatelet::system::podman::PodmanPodStatus;
 use crate::spec::cert::ClusterIssuer;
@@ -23,18 +19,16 @@ use crate::util::{hash_k8s_resource, metadata_name, NamespacedName, SkateLabels,
 use anyhow::anyhow;
 use async_trait::async_trait;
 use colored::Colorize;
-use fs2::total_space;
-use itertools::{Either, Itertools};
+use itertools::Itertools;
 use k8s_openapi::api::apps::v1::{DaemonSet, Deployment, RollingUpdateDeployment};
 use k8s_openapi::api::batch::v1::CronJob;
-use k8s_openapi::api::core::v1::{Node as K8sNode, Pod, Secret, Service};
+use k8s_openapi::api::core::v1::{Pod, Secret, Service};
 use k8s_openapi::api::networking::v1::Ingress;
 use k8s_openapi::Metadata;
 use std::cmp::Ordering;
 use std::collections::{BTreeMap, HashMap};
 use std::error::Error;
 use std::ops::Deref;
-use std::rc::Rc;
 
 #[derive(Debug)]
 pub struct ScheduleResult {
@@ -53,10 +47,7 @@ pub trait Scheduler {
 }
 
 pub struct DefaultScheduler {
-    sorter: Box<dyn QueueSort>,
-    pre_filters: Vec<Box<dyn PreFilter>>,
-    filters: Vec<Box<dyn Filter>>,
-    scorers: Vec<Box<dyn Score>>,
+    pod_scheduler: PodScheduler,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -135,17 +126,8 @@ pub struct NodeSelection {
 // distributed (pod, cron)
 impl DefaultScheduler {
     pub fn new() -> Self {
-        let node_resources_fit = Rc::new(Box::new(node_resources_fit::NodeResourcesFit {}));
         DefaultScheduler {
-            sorter: Box::new(PrioritySort {}),
-            pre_filters: vec![Box::new(NodeResourcesFit {})],
-            filters: vec![
-                Box::new(NodeNameFilter {}),
-                Box::new(NodeSelectorFilter {}),
-                Box::new(UnschedulableFilter {}),
-                Box::new(NodeResourcesFit {}),
-            ],
-            scorers: vec![Box::new(LeastPods {}), Box::new(NodeResourcesFit {})],
+            pod_scheduler: PodScheduler::new(),
         }
     }
     fn next_generation(existing: Option<&CatalogueItem>) -> i64 {
@@ -157,94 +139,6 @@ impl DefaultScheduler {
             }
         };
         1
-    }
-    fn choose_node(&self, nodes: &[NodeState], pod: &Pod) -> NodeSelection {
-        // filter nodes based on resource requirements  - cpu, memory, etc
-
-        for pre_filter in &self.pre_filters {
-            if let Err(e) = pre_filter.pre_filter(pod, nodes) {
-                return NodeSelection {
-                    selected: None,
-                    rejected: vec![RejectedNode {
-                        node_name: "*".to_string(),
-                        reason: e.to_string(),
-                    }],
-                };
-            }
-        }
-
-        let (filtered_nodes, rejected_nodes): (Vec<_>, Vec<_>) = nodes.iter().partition_map(|n| {
-            // apply all filters
-            for filter in &self.filters {
-                if let Err(e) = filter.filter(pod, n) {
-                    return Either::Right(RejectedNode {
-                        node_name: n.node_name.clone(),
-                        reason: e,
-                    });
-                }
-            }
-
-            // if all filters pass, return the node
-            Either::Left(n.clone())
-        });
-
-        if filtered_nodes.is_empty() {
-            return NodeSelection {
-                selected: None,
-                rejected: rejected_nodes,
-            };
-        }
-
-        let mut node_score_total = BTreeMap::<String, u32>::new();
-
-        for scorer in &self.scorers {
-            let mut scored_nodes = BTreeMap::<String, u32>::new();
-            for node in &filtered_nodes {
-                match scorer.score(pod, node) {
-                    Err(e) => {
-                        return NodeSelection {
-                            selected: None,
-                            rejected: vec![RejectedNode {
-                                node_name: node.node_name.clone(),
-                                reason: e.to_string(),
-                            }],
-                        }
-                    }
-                    Ok(score) => scored_nodes.insert(node.node_name.clone(), score),
-                };
-            }
-            if let Err(e) = scorer.normalize_scores(&mut scored_nodes) {
-                return NodeSelection {
-                    selected: None,
-                    rejected: vec![RejectedNode {
-                        node_name: "*".to_string(),
-                        reason: e.to_string(),
-                    }],
-                };
-            }
-
-            for (node_name, score) in scored_nodes {
-                let total_score = node_score_total.entry(node_name).or_insert(0);
-                *total_score += score;
-            }
-        }
-
-        // Find the node with the highest score
-        let (feasible_node, _) = node_score_total
-            .iter()
-            .max_by(|(_, score1), (_, score2)| score1.cmp(score2))
-            .unwrap();
-
-        NodeSelection {
-            selected: Some(
-                nodes
-                    .iter()
-                    .find(|n| n.node_name == *feasible_node)
-                    .unwrap()
-                    .clone(),
-            ),
-            rejected: rejected_nodes,
-        }
     }
 
     fn plan_daemonset(state: &ClusterState, ds: &DaemonSet) -> Result<ApplyPlan, Box<dyn Error>> {
@@ -1061,7 +955,29 @@ impl DefaultScheduler {
                             None => {
                                 match op.resource {
                                     SupportedResources::Pod(ref pod) => {
-                                        self.choose_node(&state.nodes, pod)
+                                        self.pod_scheduler.choose_node(&state.nodes, pod)
+                                    }
+                                    SupportedResources::CronJob(ref cron) => {
+                                        let pod_spec = cron
+                                            .spec
+                                            .as_ref()
+                                            .and_then(|s| s.job_template.spec.clone())
+                                            .and_then(|s| s.template.spec);
+                                        if let Some(pod_spec) = pod_spec {
+                                            let pod = Pod {
+                                                metadata: cron.metadata.clone(),
+                                                spec: Some(pod_spec),
+                                                status: None,
+                                            };
+
+                                            self.pod_scheduler.choose_node(&state.nodes, &pod)
+                                        } else {
+                                            return Err(anyhow!(
+                                                "failed to find pod spec in cron job {}",
+                                                cron.metadata.name.clone().unwrap_or_default()
+                                            )
+                                            .into());
+                                        }
                                     }
                                     _ => {
                                         // we should not get here
