03705ac330d42581bef6436cbe3c89a90dedee5f
Donal Byrne <byrnedo@tcd.ie>
2025-06-26 14:52:48 +0200

    wip

--- images/coredns/main.go
+++ images/coredns/main.go
@@ -7,12 +7,13 @@ import (
 	"github.com/coredns/coredns/core/dnsserver"
 	_ "github.com/coredns/coredns/core/plugin"
 	"github.com/coredns/coredns/coremain"
-	_ "github.com/networkservicemesh/fanout"
-	_ "github.com/openshift/coredns-mdns"
+// 	_ "github.com/networkservicemesh/fanout"
+// 	_ "github.com/openshift/coredns-mdns"
+	_ "github.com/ziollek/gathersrv"
 )
 
 func init() {
-	dnsserver.Directives = append(dnsserver.Directives, "mdns", "fanout")
+	dnsserver.Directives = append(dnsserver.Directives, "gathersrv")
 }
 
 func main() {
