536511df26bf456acf0b652dad7e1a6e76e0fd10
Donal Byrne <byrnedo@tcd.ie>
2025-07-07 23:55:28 +0200

    Gathersrv example working

--- images/coredns/main.go
+++ images/coredns/main.go
@@ -4,16 +4,27 @@ package main
 // This enables us to use things like go mod tidy
 
 import (
+	"slices"
+
 	"github.com/coredns/coredns/core/dnsserver"
 	_ "github.com/coredns/coredns/core/plugin"
 	"github.com/coredns/coredns/coremain"
-// 	_ "github.com/networkservicemesh/fanout"
-// 	_ "github.com/openshift/coredns-mdns"
+	// 	_ "github.com/networkservicemesh/fanout"
+	// 	_ "github.com/openshift/coredns-mdns"
 	_ "github.com/ziollek/gathersrv"
 )
 
 func init() {
-	dnsserver.Directives = append(dnsserver.Directives, "gathersrv")
+
+	index := 0
+	for i, plugin := range dnsserver.Directives {
+		if plugin == "prometheus" {
+			index = i
+			break
+		}
+	}
+
+	dnsserver.Directives = slices.Insert(dnsserver.Directives, index+1, "gathersrv")
 }
 
 func main() {
