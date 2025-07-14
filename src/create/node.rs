77bd9df5ff47be6e5e310674aa944dd191c32939
Donal Byrne <byrnedo@tcd.ie>
2025-07-14 23:37:16 +0200

    Enable and start podman-restart.service

--- src/create/node.rs
+++ src/create/node.rs
@@ -244,6 +244,9 @@ pub async fn create_node<D: CreateDeps>(deps: &D, args: CreateNodeArgs) -> Resul
         }
     }
 
+    // enable the restart service
+    conn.execute_stdout("sudo systemctl enable podman-restart.service && sudo systemctl start podman-restart.service", true, true).await?;
+
     // seems to be missing when using kube play
     // TODO - might not need this anymore
     let cmd =
