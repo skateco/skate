f5c039246ad82894be47d8a12144035257e68351
Donal Byrne <byrnedo@tcd.ie>
2025-07-29 21:56:26 +0200

    List peers

--- src/skatelet/database/peer.rs
+++ src/skatelet/database/peer.rs
@@ -35,7 +35,7 @@ pub async fn upsert_peer(db: impl SqliteExecutor<'_>, peer: &Peer) -> super::Res
 pub async fn list_peers(db: impl SqliteExecutor<'_>) -> super::Result<Vec<Peer>> {
     let peers = sqlx::query_as!(
         Peer,
-        r#" SELECT id as "id!: i64", node_name as "name!: String",  created_at as "created_at!: DateTime<Local>", updated_at as "updated_at!: DateTime<Local>",
+        r#" SELECT id as "id!: i64", node_name as "node_name!: String", ip_address as "ip_address!: String",  created_at as "created_at!: DateTime<Local>", updated_at as "updated_at!: DateTime<Local>"
             FROM peers
         "#,
     )
